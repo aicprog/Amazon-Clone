ad54cedd6afe1af93747e0d9974fd7fd1a80396b
Aicprog <aicprog@gmail.com>
2021-11-10 22:43:35 -0800

    deleted redundant files from merge

--- Components/index.js
+++
@@ -1,6 +0,0 @@
-
-//Home Page
-export {default as Header} from './Home/Header' 
-export {default as Footer} from './Home/Footer' 
-export {default as Carousel} from './Home/Carousel' 
-export {default as ProductRow} from './Home/ProductRow' 
